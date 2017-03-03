8ad82732969ec157856fd10bea1f862be312e2ee
talithanurinaaa <inastalitha00@gmail.com>
2017-03-03 14:24:49 +0700

    terserah

---
+++ exaray.php
@@ -0,0 +1,13 @@
+<?php
+
+$mhs["charlie"]=90;
+$mhs["ani"]=56;
+$mhs["budi"]=52;
+$mhs["shinta"]=99;
+
+echo "nilai : <br>";
+echo "nilai matkul pemrograman dari rudi adalah ".$mhs["charlie"]."<br>";
+echo "nilai matkul pemrograman dari anto adalah ".$mhs["ani"]."<br>";
+echo "nilai matkul pemrograman dari joko adalah ".$mhs["budi"]."<br>";
+echo "nilai matkul pemrograman dari sinta adalah ".$mhs["shinta"]."<br>"
+?>
